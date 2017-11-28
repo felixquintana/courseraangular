3ce42a12491b1503e48797b342156fb0d22e1c5f
felix alberto quintana <felixalbertoquintana@gmail.com>
2017-11-28 14:03:19 +0100

    index.html

---
+++ app.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+angular.module(miPrimeraAplicacion, [])
+.controller('MiPrimerControlador', function (~scope) {
+  $scope.name="Felix";
+  $scope.sayHello = function()  {
+    return "Hello Coursera Script";
+
+  };
+});
+})();
